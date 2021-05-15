122133f381c32a37de7d9fc0e0ec52e3fe4761e7
Jakob Brkic <jale.pnv@gmail.com>
2021-05-15 12:05:25 +0100

    Table projekt

--- firmajp24.sql
+++ firmajp24.sql
@@ -1,3 +1,9 @@
 DROP database IF EXISTS firmajp24;
 CREATE database firmajp24;
 use firmajp24;
+
+CREATE TABLE projekt(
+sifra int NOT NULL PRIMARY KEY auto_increment,
+naziv varchar(50) NOT NULL,
+cijena decimal(18,2)
+);
