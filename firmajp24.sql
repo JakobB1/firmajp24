f7e2b7cbbe6b9a7f8347e767c1eee229763a0d58
Jakob Brkic <jale.pnv@gmail.com>
2021-05-15 12:15:38 +0100

    Table sudjeluje

--- firmajp24.sql
+++ firmajp24.sql
@@ -12,6 +12,13 @@ CREATE TABLE programer(
 sifra int NOT NULL PRIMARY KEY auto_increment,
 ime varchar(50) NOT NULL,
 prezime varchar (50) NOT NULL,
-datum rodenja datetime,
+datumrodenja datetime,
 placa decimal(18,2) NULL
+);
+
+CREATE TABLE sudjeluje(
+projekt int,
+programer int,
+datumpocetka datetime,
+datum kraja datetime
 );
