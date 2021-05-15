2f75e40813869f18a08bc0ee7189a987bbda0902
Jakob Brkic <jale.pnv@gmail.com>
2021-05-15 12:12:50 +0100

    Table programer

--- firmajp24.sql
+++ firmajp24.sql
@@ -7,3 +7,11 @@ sifra int NOT NULL PRIMARY KEY auto_increment,
 naziv varchar(50) NOT NULL,
 cijena decimal(18,2)
 );
+
+CREATE TABLE programer(
+sifra int NOT NULL PRIMARY KEY auto_increment,
+ime varchar(50) NOT NULL,
+prezime varchar (50) NOT NULL,
+datum rodenja datetime,
+placa decimal(18,2) NULL
+);
